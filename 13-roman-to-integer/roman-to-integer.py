71dc50a5139b65d90e846075ab9d177b576bfe56
AMAR DEEP s <138798428+amargauge26@users.noreply.github.com>
2024-09-12 15:35:21 +0530

    Time: 56 ms (11.03%) | Memory: 16.7 MB (7.57%) - LeetSync

--- 13-roman-to-integer/roman-to-integer.py
+++ 13-roman-to-integer/roman-to-integer.py
@@ -11,3 +11,15 @@ class Solution:
                 num+=f[s[i]]
         
         return num+f[s[-1]]
+        
+def romanToInt(self, s):
+        adict={"I":1,"V":5,"X":10,"L":50,"C":100,"D":500,"M":1000}
+        digits=list(s.upper())  
+        post=0
+        pre=0
+        for i in range(0,len(digits)):
+            if (i<len(digits)-1) and (adict[digits[i]] < adict[digits[i+1]]):
+                pre+=adict[digits[i]]
+            else:
+                post+= adict[digits[i]]
+        return post - pre  
